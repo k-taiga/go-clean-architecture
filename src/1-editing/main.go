cb8847fe1dd760161ed58846b28f73752e2d47c9
K-taiga <taigakatou0329@gmail.com>
2023-12-09 18:00:00 +0900

    [add] update user

--- src/1-editing/main.go
+++ src/1-editing/main.go
@@ -27,6 +27,19 @@ func initDB(filepath string) *sql.DB {
 	return db
 }
 
+func validateUser(name string, age int) error {
+	if name == "" {
+		return echo.NewHTTPError(http.StatusBadRequest, "name is empty")
+	}
+	if len(name) > 100 {
+		return echo.NewHTTPError(http.StatusBadRequest, "name is too long")
+	}
+	if age < 0 || age >= 200 {
+		return echo.NewHTTPError(http.StatusBadRequest, "age must be between 0 and 200")
+	}
+	return nil
+}
+
 func main() {
 	db := initDB("example.db")
 	e := echo.New()
@@ -72,6 +85,42 @@ func main() {
 			users = append(users, user)
 		}
 		return c.JSON(http.StatusOK, users)
+	})
+
+	e.PUT("/users/:id", func(c echo.Context) error {
+		id, err := strconv.Atoi(c.Param("id"))
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		name := c.FormValue("name")
+		age, err := strconv.Atoi(c.FormValue("age"))
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		// 右側がif文の条件式で左側がその実行式の前に実行するもの
+		if err := validateUser(name, age); err != nil {
+			return err
+		}
+
+		result, err := db.Exec("UPDATE users SET name = ?, age = ? where id = ?", name, age, id)
+
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		// RowsAffected()で変更されたレコード数を取得
+		rows, _ := result.RowsAffected()
+
+		if rows == 0 {
+			return echo.NewHTTPError(http.StatusNotFound, "not found")
+		}
+
+		// &Userはnew(User)とほぼ同じで生成されたオブジェクトのポインタを返すがstructの中身を指定して生成できる
+		return c.JSON(http.StatusOK, &User{ID: id, Name: name, Age: age})
 	})
 
 	e.Start(":8080")
